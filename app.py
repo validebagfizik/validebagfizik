7e2922f889c7b8ddad416e48ecdffd711a77170c
validebagfizik <97658907+validebagfizik@users.noreply.github.com>
2026-08-13 13:48:56 +0300

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, redirect
-
-app = Flask(__name__)
-
-TRT1_LINK = "https://trt.daioncdn.net/trt-1/master.m3u8?app=web"
-
-@app.route('/trt1.m3u8')
-def trt1():
-    return redirect(TRT1_LINK)
